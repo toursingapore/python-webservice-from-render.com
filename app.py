d3d7ada0feb194db4ea64240de9af4beb7f67029
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-04-19 14:01:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+@app.route("/")
+def index():
+    celsius = str(escape(request.args.get("celsius", "")))
+    return (
+        """<form action="" method="get">
+                <input type="text" name="celsius">
+                <input type="submit" value="Convert">
+            </form>"""
+        + celsius
+    )
+
+@app.route("/<int:celsius>")
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    fahrenheit = float(celsius) * 9 / 5 + 32
+    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+    return str(fahrenheit)
 
 
 if __name__ == "__main__":
