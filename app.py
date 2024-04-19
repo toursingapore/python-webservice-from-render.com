e7afc969dd2159fa68ea99d0b653bf0a6ea9cedb
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-04-19 14:16:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 from flask import Flask
-from flask import request, escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    celsius = str(escape(request.args.get("celsius", "")))
-    return (
-        """<form action="" method="get">
-                <input type="text" name="celsius">
-                <input type="submit" value="Convert">
-            </form>"""
-        + celsius
-    )
-
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == "__main__":
     app.run(debug=True)
