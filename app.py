ea605cd39d800db9b456b406846822aaf6601bfe
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-04-19 13:48:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ app = Flask(__name__)
 @app.route('/')
 #def hello_world():
 #    return 'Hello World, welcome to you!'
-
-def my_func_doing(number1, number2):
-    return number1 + number2
-
-number1 = float(input("Enter the first number: "))
-number2 = float(input("Enter the second number: "))
-solution = do_calculation(number1, number2)
-print("The sum of your numbers is {}".format(solution))
+    return '''
+        <html>
+            <body>
+                {errors}
+                <p>Enter your numbers:</p>
+                <form method="post" action=".">
+                    <p><input name="number1" /></p>
+                    <p><input name="number2" /></p>
+                    <p><input type="submit" value="Do calculation" /></p>
+                </form>
+            </body>
+        </html>
+    '''.format(errors=errors)
 
 
 if __name__ == "__main__":
