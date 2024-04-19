e15c12160313b85e435caf237db3a0c654c852ee
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-04-19 13:54:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-#def hello_world():
-#    return 'Hello World, welcome to you!'
-    return '''
-        <html>
-            <body>
-                {errors}
-                <p>Enter your numbers:</p>
-                <form method="post" action=".">
-                    <p><input name="number1" /></p>
-                    <p><input name="number2" /></p>
-                    <p><input type="submit" value="Do calculation" /></p>
-                </form>
-            </body>
-        </html>
-    '''.format(errors=errors)
+def hello_world():
+    return 'Hello from Flask!'
 
 
 if __name__ == "__main__":
