f53a35cb6956ac53a66fb37d2e3c82885bbd79e2
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-04-19 13:43:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World, welcome to you!'
+#def hello_world():
+#    return 'Hello World, welcome to you!'
+
+def my_func_doing(number1, number2):
+    return number1 + number2
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+solution = do_calculation(number1, number2)
+print("The sum of your numbers is {}".format(solution))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
